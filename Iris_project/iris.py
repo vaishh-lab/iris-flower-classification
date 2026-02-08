885415ae55a178224a2fe8087bf55f43d2cea322
vaishh-lab <vn256833@gmail.com>
2026-02-08 23:46:47 +0530

    Add files via upload

---
+++ Iris_project/iris.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Accuracy
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Model Accuracy:", accuracy)
+
+# User input
+print("\nEnter flower details:")
+sl = float(input("Sepal Length: "))
+sw = float(input("Sepal Width: "))
+pl = float(input("Petal Length: "))
+pw = float(input("Petal Width: "))
+
+sample = [[sl, sw, pl, pw]]
+prediction = model.predict(sample)
+
+print("\n🌸 Predicted Flower:", iris.target_names[prediction[0]])
